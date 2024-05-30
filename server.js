9fabf3ce1b8f09035ca7bdbfb7afa32edbf98188
pedro cangemi <pedrocangemi@gmail.com>
2024-05-30 11:34:06 -0300

    payload ajustado

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 let sensorData = {
   temperature: null,
